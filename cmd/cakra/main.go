1ef9f9c63be06f65a2ad5229118ce38debbbdd71
Erwin Kurniawan <rootshaxor@gmail.com>
2026-08-08 09:49:56 +0700

    init: bootstrap CakraOS core

---
+++ cmd/cakra/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	name    = "CakraOS"
+	version = "0.1.0"
+)
+
+func main() {
+	fmt.Printf("%s Core\n", name)
+	fmt.Printf("Version: %s\n", version)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CakraOS/cakra
+
+go 1.26.3
